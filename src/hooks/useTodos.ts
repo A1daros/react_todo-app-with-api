50f9a793403438881100e288471be42d1f704314
Anatolii Lutai <toliklutai@gmail.com>
2026-05-07 18:24:06 +0300

    feat: create enum ErrorMessage and re-use them to avoid hard-coded types

---
+++ src/hooks/useTodos.ts
@@ -0,0 +1,73 @@
+import { useEffect, useState } from 'react';
+import { Todo } from '../types/Todo';
+import { createTodo, deleteTodo, USER_ID } from '../api/todos';
+import { client } from '../utils/fetchClient';
+import { ErrorMessage } from '../types/types';
+
+export const useTodos = (onError: (message: string) => void) => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [tempTodo, setTempTodo] = useState<Todo | null>(null);
+  const [deletingId, setDeletingId] = useState<number[]>([]);
+
+  useEffect(() => {
+    client
+      .get<Todo[]>(`/todos?userId=${USER_ID}`)
+      .then(setTodos)
+      .catch(() => onError(ErrorMessage.LoadTodo));
+  }, [onError]);
+
+  const addTodo = async (title: string) => {
+    const trimmedTitle = title.trim();
+
+    setTempTodo({
+      id: 0,
+      title: trimmedTitle,
+      completed: false,
+      userId: USER_ID,
+    });
+
+    try {
+      const newTodo = await createTodo({
+        title: trimmedTitle,
+        completed: false,
+        userId: USER_ID,
+      });
+
+      setTodos(prev => [...prev, newTodo]);
+    } catch {
+      onError(ErrorMessage.AddTodo);
+      throw new Error();
+    } finally {
+      setTempTodo(null);
+    }
+  };
+
+  const removeTodo = async (todoId: number) => {
+    setDeletingId(prev => [...prev, todoId]);
+
+    try {
+      await deleteTodo(todoId);
+      setTodos(prev => prev.filter(todo => todo.id !== todoId));
+    } catch {
+      onError(ErrorMessage.DeleteTodo);
+    } finally {
+      setDeletingId(prev => prev.filter(id => id !== todoId));
+    }
+  };
+
+  const clearCompleted = async () => {
+    const completedTodos = todos.filter(todo => todo.completed);
+
+    await Promise.all(completedTodos.map(todo => removeTodo(todo.id)));
+  };
+
+  return {
+    todos,
+    tempTodo,
+    deletingId,
+    addTodo,
+    removeTodo,
+    clearCompleted,
+    setTodos,
+  };
+};
---
+++ src/types/types.ts
@@ -0,0 +1,11 @@
+export enum FilterStatus {
+  All = 'all',
+  Active = 'active',
+  Completed = 'completed',
+}
+
+export enum ErrorMessage {
+  LoadTodo = 'Unable to load todos',
+  AddTodo = 'Unable to add a todo',
+  DeleteTodo = 'Unable to delete a todo',
+}
